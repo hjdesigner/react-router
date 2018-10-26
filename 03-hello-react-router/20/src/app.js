115b95fa9a5eceda207ac57732cd7c52f0f89317
Henrique Melanda <henrique.melanda@fcamara.com.br>
2018-10-26 13:53:40 -0300

    withRouter, hashRouter and Route not render

---
+++ 03-hello-react-router/20/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import MainApp from './main-app'
+
+const App = () => (
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
+)
+
+export default App
